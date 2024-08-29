2fd12ce65a275db7cf4d26deea8f0d4c70f84234
pateld1p <111032586+pateld1p@users.noreply.github.com>
2024-08-28 22:01:45 -0700

    stripe and error handling

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
@@ -5,6 +7,32 @@ import { Container, AppBar, Toolbar, Typography, Button, Box, Grid } from "@mui/
 import Head from "next/head";
 
 export default function Home() {
+
+  const handleSubmit = async () => {
+    const checkoutSession = await fetch('/api/checkout_session', {
+      method: 'POST',
+      headers: {
+        origin: 'http://localhost:3000',
+      }, 
+    })
+
+    const checkoutSessionJson = await checkoutSession.json()
+
+    if(checkoutSession.statusCode === 500){
+      console.error(checkoutSession.message)
+    return
+    }
+  
+  const stripe = await getStripe()
+  const {error} = await stripe.redirectToCheckout({
+    sessionId: checkoutSessionJson.id
+  })
+
+  if (error){
+    console.warn(error.message)
+  }
+}
+
   return (
     <Container maxWidth="100v">
       <Head>
@@ -98,7 +126,7 @@ export default function Home() {
                 {' '}
                 Unlimited flashcards and storage, with priority support.
               </Typography>
-              <Button variant="contained" color="primary" sx={{mt:2}}>Choose Pro</Button>
+              <Button variant="contained" color="primary" sx={{mt:2}} onClick={handleSubmit}>Choose Pro</Button>
             </Box>
           </Grid>
         </Grid>
--- firebase.js
+++ firebase.js
@@ -8,13 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCq7wyg8akZT-vB_iIgP6AqHZ0cxOl2M3Q",
-  authDomain: "flashcardsaas-52f43.firebaseapp.com",
-  projectId: "flashcardsaas-52f43",
-  storageBucket: "flashcardsaas-52f43.appspot.com",
-  messagingSenderId: "114583786991",
-  appId: "1:114583786991:web:b15996fce7ff79582fd21a",
-  measurementId: "G-Z0LMH35KF4"
+
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
